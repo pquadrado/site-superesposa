4c5b678246fd3391ecaa033d5756adcd9b093151
Lucas <lucas.g.apostolo@gmail.com>
2024-11-12 14:28:50 -0300

    feat: forms emailjs trabalhe conosco

--- js/webflow.js
+++ js/webflow.js
@@ -103,23 +103,77 @@ document.getElementById("cotarAgora").addEventListener("click", function (event)
 });
 
 
-document.getElementById("button-4-horas").addEventListener("click", function (event){
+document.getElementById("button-4-horas").addEventListener("click", function (event) {
   event.preventDefault()
   const mensagem = "Eu vim pelo site e quero um orçamento para uma limpeza de 4 horas"
   const urlWhatsApp = `https://wa.me/5516996276515?text=${encodeURIComponent(mensagem)}`
   window.open(urlWhatsApp, "_blank")
 })
 
-document.getElementById("button-6-horas").addEventListener("click", function (event){
+document.getElementById("button-6-horas").addEventListener("click", function (event) {
   event.preventDefault()
   const mensagem = "Eu vim pelo site e quero um orçamento para uma limpeza de 6 horas"
   const urlWhatsApp = `https://wa.me/5516996276515?text=${encodeURIComponent(mensagem)}`
   window.open(urlWhatsApp, "_blank")
 })
 
-document.getElementById("button-8-horas").addEventListener("click", function (event){
+document.getElementById("button-8-horas").addEventListener("click", function (event) {
   event.preventDefault()
   const mensagem = "Eu vim pelo site e quero um orçamento para uma limpeza de 8 horas"
   const urlWhatsApp = `https://wa.me/5516996276515?text=${encodeURIComponent(mensagem)}`
   window.open(urlWhatsApp, "_blank")
+})
+
+
+// Configuração do formulários
+
+let estadoCivilSelecionado
+let filhosSelecionado
+let conhecimentoDiarista
+
+document.querySelectorAll('input[name="estadoCivil"]').forEach((radio) => {
+  radio.addEventListener('change', function () {
+    estadoCivilSelecionado = this.value;
+  });
+});
+
+document.querySelectorAll('input[name="filhos"]').forEach((radio) => {
+  radio.addEventListener('change', function () {
+    filhosSelecionado = this.value;
+  });
+});
+
+document.querySelectorAll('input[name="conhecimentoDiarista"]').forEach((radio) => {
+  radio.addEventListener('change', function () {
+    conhecimentoDiarista = this.value;
+  });
+});
+
+
+function sendEmailTrabalheConosco() {
+  const params = {
+    name: document.getElementById("name").value,
+    endereco: document.getElementById("endereco").value,
+    contato: document.getElementById("telefone").value,
+    data_nascimento: document.getElementById("datanascimento").value,
+    civil: estadoCivilSelecionado,
+    filhos: filhosSelecionado,
+    quantidade: document.getElementById("quantosfilhos").value,
+    diarista: conhecimentoDiarista,
+    tempo_diarista: document.getElementById("tempo_diarista").value
+  }
+
+  const serviceId = "service_t4cb82l"
+  const templateId = "template_n4sp1zr"
+
+  emailjs.send(serviceId, templateId, params).then(
+    (res) => {
+      alert("Foi enviado com sucesso!")
+    }
+  ).catch((err) => console.log(err))
+}
+
+document.getElementById("email-form").addEventListener("submit", function (event) {
+  event.preventDefault()
+  sendEmail()
 })
